171cf645c9c480f141baafb7d7e8481e9d415a23
totosch <totokennedy@gmail.com>
2026-02-14 20:08:33 -0300

    added s3 in minio form

--- src/main/java/com/rsch/service/FileService.java
+++ src/main/java/com/rsch/service/FileService.java
@@ -5,49 +5,62 @@ import com.rsch.exception.FileEntityNotFoundException;
 import com.rsch.model.User;
 import com.rsch.model.UserFile;
 import com.rsch.repository.UserFileRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.kafka.core.KafkaTemplate;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.UUID;
 
 @Service
 public class FileService {
 
     private final UserFileRepository userFileRepository;
-    private final String FOLDER_PATH = "/home/schillaci/Desktop/FilesPath/";
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public FileService(UserFileRepository userFileRepository, KafkaTemplate<String, String> kafkaTemplate) {
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    public FileService(UserFileRepository userFileRepository, KafkaTemplate<String, String> kafkaTemplate, S3Client s3Client) {
         this.userFileRepository = userFileRepository;
         this.kafkaTemplate = kafkaTemplate;
+        this.s3Client = s3Client;
     }
 
     public FileResponse uploadFile(MultipartFile file, User user) throws IOException {
-        Files.createDirectories(Paths.get(FOLDER_PATH));
-        String nameOfFileName = file.getOriginalFilename();
+        String originalFilename = file.getOriginalFilename();
         String fileType = file.getContentType();
         Long fileSize = file.getSize();
 
+        String s3Key = UUID.randomUUID() + "_" + originalFilename;
 
-        String filePath = FOLDER_PATH + nameOfFileName;
-        file.transferTo(new File(filePath));
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(s3Key)
+                .contentType(fileType)
+                .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), fileSize));
 
         UserFile fileToSave = new UserFile(
-                nameOfFileName,
+                originalFilename,
                 fileType,
                 fileSize,
-                filePath,
+                s3Key,
                 user
         );
 
@@ -81,21 +94,27 @@ public class FileService {
                 .orElseThrow(() -> new FileEntityNotFoundException("file not found with id: " + id));
     }
 
-    public Resource loadFileAsResource(String filePath) throws MalformedURLException, FileNotFoundException {
-        Path path = Paths.get(filePath);
-        Resource resource = new UrlResource(path.toUri());
+    public Resource loadFileAsResource(String s3Key) throws FileNotFoundException {
+        try {
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(s3Key)
+                    .build();
 
-        if(resource.exists()) {
-            return resource;
-        } else {
-            throw new FileNotFoundException("file not found: " + filePath);
+            return new InputStreamResource(s3Client.getObject(getObjectRequest));
+
+        } catch (NoSuchKeyException e) {
+            throw new FileNotFoundException("File not found in S3 bucket: " + s3Key);
         }
     }
 
-    public void deleteFile(Integer id, String filePath) throws IOException {
-        Files.deleteIfExists(Paths.get(filePath));
+    public void deleteFile(Integer id, String s3Key) {
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(s3Key)
+                .build();
+        s3Client.deleteObject(deleteObjectRequest);
 
         userFileRepository.deleteById(id);
     }
-
 }
